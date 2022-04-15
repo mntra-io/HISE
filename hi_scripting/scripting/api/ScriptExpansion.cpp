0e66047a5218e1bcd77b5fcfe1eee26d7d48f9b4
dave <dave41264@hotmail.com>
2022-04-15 01:29:06 +0100

    Initialise pools for full expansions

--- hi_scripting/scripting/api/ScriptExpansion.cpp
+++ hi_scripting/scripting/api/ScriptExpansion.cpp
@@ -1616,6 +1616,12 @@ juce::Result FullInstrumentExpansion::initialise()
 			return Result::fail("Error parsing hxi file");
 
 		jassert(allData.isValid() && allData.getType() == ExpansionIds::FullData);
+		
+		for (auto fileType : getListOfPooledSubDirectories())
+		{
+			setCompressorForPool(fileType, true);
+			restorePool(allData, fileType);
+		}
 
 		auto networkData = allData.getChildWithName("Networks");
 
