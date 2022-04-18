9cb405f591795bad6775ef96e51c888d2406207a
dave <dave41264@hotmail.com>
2022-04-18 21:15:53 +0100

    Added Engine.intToHexString

--- hi_scripting/scripting/api/ScriptingApi.cpp
+++ hi_scripting/scripting/api/ScriptingApi.cpp
@@ -874,6 +874,7 @@ struct ScriptingApi::Engine::Wrapper
 	API_METHOD_WRAPPER_2(Engine, matchesRegex);
 	API_METHOD_WRAPPER_2(Engine, getRegexMatches);
 	API_METHOD_WRAPPER_2(Engine, doubleToString);
+	API_METHOD_WRAPPER_1(Engine, intToHexString);
 	API_METHOD_WRAPPER_0(Engine, getOS);
 	API_METHOD_WRAPPER_0(Engine, getSystemStats);
 	API_METHOD_WRAPPER_0(Engine, isPlugin);
@@ -997,6 +998,7 @@ parentMidiProcessor(dynamic_cast<ScriptBaseMidiProcessor*>(p))
 	ADD_API_METHOD_2(matchesRegex);
 	ADD_API_METHOD_2(getRegexMatches);
 	ADD_API_METHOD_2(doubleToString);
+	ADD_API_METHOD_1(intToHexString);
 	ADD_API_METHOD_1(getMasterPeakLevel);
 	ADD_API_METHOD_0(getOS);
 	ADD_API_METHOD_0(getSystemStats);
@@ -2567,6 +2569,12 @@ String ScriptingApi::Engine::doubleToString(double value, int digits)
 {
     return String(value, digits);
 }
+
+String ScriptingApi::Engine::intToHexString(int value)
+{
+    return String::toHexString(value);
+}
+
 
 void ScriptingApi::Engine::quit()
 {
--- hi_scripting/scripting/api/ScriptingApi.h
+++ hi_scripting/scripting/api/ScriptingApi.h
@@ -548,6 +548,8 @@ public:
 
     /** Returns a string of the value with the supplied number of digits. */
     String doubleToString(double value, int digits);
+		
+		String intToHexString(int value);
 
 		/** Signals that the application should terminate. */
 		void quit();
