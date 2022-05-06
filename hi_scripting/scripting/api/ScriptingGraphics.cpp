d81d4ed73e1ecdd0c7853b8a4b70c768f04ec242
dave <dave41264@hotmail.com>
2022-05-06 14:24:50 +0100

    Added enabled property to table LAF

--- hi_scripting/scripting/api/ScriptingGraphics.cpp
+++ hi_scripting/scripting/api/ScriptingGraphics.cpp
@@ -2236,6 +2236,8 @@ void ScriptingObjects::ScriptedLookAndFeel::Laf::drawTableBackground(Graphics& g
 		obj->setProperty("area", ApiHelpers::getVarRectangle(area));
 		obj->setProperty("id", te.getName());
 		obj->setProperty("position", rulerPosition);
+		obj->setProperty("enabled", te.isEnabled());
+		
 		setColourOrBlack(obj, "bgColour",    te, TableEditor::ColourIds::bgColour);
 		setColourOrBlack(obj, "itemColour",  te, TableEditor::ColourIds::fillColour);
 		setColourOrBlack(obj, "itemColour2", te, TableEditor::ColourIds::lineColour);
@@ -2264,7 +2266,8 @@ void ScriptingObjects::ScriptedLookAndFeel::Laf::drawTablePath(Graphics& g_, Tab
 
 		obj->setProperty("area", ApiHelpers::getVarRectangle(area));
 		obj->setProperty("lineThickness", lineThickness);
-
+		obj->setProperty("enabled", te.isEnabled());
+		
 		setColourOrBlack(obj, "bgColour", te, TableEditor::ColourIds::bgColour);
 		setColourOrBlack(obj, "itemColour", te, TableEditor::ColourIds::fillColour);
 		setColourOrBlack(obj, "itemColour2", te, TableEditor::ColourIds::lineColour);
@@ -2289,7 +2292,8 @@ void ScriptingObjects::ScriptedLookAndFeel::Laf::drawTablePoint(Graphics& g_, Ta
 		obj->setProperty("isEdge", isEdge);
 		obj->setProperty("hover", isHover);
 		obj->setProperty("clicked", isDragged);
-
+		obj->setProperty("enabled", te.isEnabled());
+		
 		setColourOrBlack(obj, "bgColour", te, TableEditor::ColourIds::bgColour);
 		setColourOrBlack(obj, "itemColour", te, TableEditor::ColourIds::fillColour);
 		setColourOrBlack(obj, "itemColour2", te, TableEditor::ColourIds::lineColour);
@@ -2313,6 +2317,8 @@ void ScriptingObjects::ScriptedLookAndFeel::Laf::drawTableRuler(Graphics& g_, Ta
 		obj->setProperty("area", ApiHelpers::getVarRectangle(area));
 		obj->setProperty("position", rulerPosition);
 		obj->setProperty("lineThickness", lineThickness);
+		obj->setProperty("enabled", te.isEnabled());
+		
 		setColourOrBlack(obj, "bgColour",    te, TableEditor::ColourIds::bgColour);
 		setColourOrBlack(obj, "itemColour",  te, TableEditor::ColourIds::fillColour);
 		setColourOrBlack(obj, "itemColour2", te, TableEditor::ColourIds::lineColour);
