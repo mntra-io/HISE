5a20223d18504e73b2bccdb0e8599a9c398ac4dc
MelodieSicard <103433384+MelodieSicard@users.noreply.github.com>
2023-06-26 18:50:05 -0400

    Fix exporter IPP default value

--- hi_backend/backend/ProjectTemplate.cpp
+++ hi_backend/backend/ProjectTemplate.cpp
@@ -78,7 +78,7 @@ static const unsigned char projectTemplate_jucer_lines[] =
 "  </MAINGROUP>\r\n"
 "  <EXPORTFORMATS>\r\n"
 "    <%VS_VERSION% targetFolder=\"Builds/%TARGET_FOLDER%\" vstLegacyFolder=\"%VSTSDK_FOLDER%\" vst3Folder=\"%VSTSDK3_FOLDER%\" aaxFolder=\"%AAX_PATH%\" \r\n"
-"            IPPLibrary=\"Sequential\" IPP1ALibrary=\"%IPP_1A%\" extraDefs=\"%PLUGIN_CHANNEL_AMOUNT%&#10;%EXTRA_DEFINES_WIN%\" extraCompilerFlags=\"/bigobj %MSVC_WARNINGS%\" >\r\n"
+"            IPPLibrary=\"Sequential\" IPP1ALibrary=\"true\" extraDefs=\"%PLUGIN_CHANNEL_AMOUNT%&#10;%EXTRA_DEFINES_WIN%\" extraCompilerFlags=\"/bigobj %MSVC_WARNINGS%\" >\r\n"
 "      <CONFIGURATIONS>\r\n"
 "        <CONFIGURATION name=\"Debug\" winWarningLevel=\"1\" generateManifest=\"1\" winArchitecture=\"x64\"\r\n"
 "                       isDebug=\"1\" optimisation=\"1\" targetName=\"%NAME% Debug\"\r\n"
